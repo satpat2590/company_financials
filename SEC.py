f7684a30a062f445e9b9cf7bb17853ab7c0bcb1a
Satyam Patel <patelsatyam100@gmail.com>
2026-01-18 09:33:55 -0500

    updating gitignore

--- SEC.py
+++ SEC.py
@@ -3,7 +3,7 @@ import datetime
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent, FakeUserAgent
 import requests
-from sec_cik_mapper import StockMapper 
+import pandas as pd
 from typing import Dict
 
 # Add modules from base repo
@@ -11,6 +11,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from utils.session import RequestSession
+from utils.excel_formatter import ExcelFormatter
 
 
 def save_json(spath: str, data: Dict) -> None:
@@ -22,7 +23,7 @@ def save_json(spath: str, data: Dict) -> None:
     """
     print(f"\n[OMNI] - {datetime.datetime.now()} - Saving data in {spath}...\n")
     with open(spath, 'w+') as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
 
 class SEC():
     """
@@ -34,6 +35,7 @@ class SEC():
 
      # Configuration
         self.reqsesh = RequestSession()
+        self.ef = ExcelFormatter()
         self.url_template = "https://data.sec.gov/submissions/CIK##########.json"
         self.url_xbrl_acc_payable = "https://data.sec.gov/api/xbrl/companyconcept/CIK##########/us-gaap/AccountsPayableCurrent.json"
         self.url_xbrl = "https://data.sec.gov/api/xbrl/companyfacts/CIK##########.json"
@@ -45,20 +47,63 @@ class SEC():
             self.cik_map = json.load(f)
 
         self.tickers = ['PLTR', 'BABA', 'VALE', 'WMT', 'SMCI']
+        self.tickers = ['PLTR', 'AXTI', 'GOLD']
         print(f"Printing out the following tickers: {self.tickers}")
         for ticker in self.tickers:
             gaap_record = self.fetch_sec_filing(ticker)
             if gaap_record:
-                gaap_record_cleaned = gaap_record.content.decode('utf-8')
-                print(ticker, "\n", gaap_record_cleaned)
-            
+                gaap_record_cleaned = gaap_record.json() 
+                #print(ticker, "\n", json.dumps(gaap_record_cleaned, indent=4))
+                #save_json(os.path.join(os.path.dirname(__file__), f"data\\{ticker}.json"), gaap_record_cleaned)
+                self.clean_facts(gaap_record_cleaned)
             print("\n\n")
+
+        self.ef.save(f"EDGAR_FINANCIALS_{datetime.datetime.now().strftime('%Y%m%d')}_{datetime.datetime.now().strftime('%H%M%S')}.xlsx", os.path.join(os.path.dirname(__file__), "data"))
             
 
-        #self.fetch_sec_filings(self.cik_list)
+    def clean_facts(self, json) -> pd.DataFrame:
+        """
+        Given a suite of company facts data from SEC, clean it up and then store it in some CSV file
+        
+        :param json: JSON data which pertains to the facts being brought on for the particular ticker (company)
+        """
+        cik = json.get("cik", None)
+        if not cik:
+            print(f"\nThere is no CIK for the company you passed in.")
+            return None
 
-     # Key = Ticker; Value = CIK
-        #self.ticker_mapping = pull_json(jpath) 
+        entity = json.get("entityName", None)
+        if not entity:
+            print(f"\nThere is no entityName for the data you passed in")
+            return None 
+        
+        facts = json.get("facts", None)
+        if not facts:
+            print(f"\nThere are no facts for the given entity: {entity}\n")
+            return None 
+
+        cfacts = []
+        for key, value in facts.items():
+            #print(f"\nAnalyzing the following facts: {key}...\n")
+            for field, data in value.items():
+                #print(f"Field name: {field}")
+                for metafield, attr in data.items():
+                    #if "label" == metafield:
+                     #   print(f"Label: {attr}")
+                    #if "description" == metafield:
+                     #   print(f"Description: {attr}")
+                    if "units" == metafield:
+                        for key, list in attr.items():
+                            #print(f"Grabbing all facts for the following unit: {key}...")
+                            for obj in list:
+                             # Columns: [CIK, EntityName, Timestamp, Value, AccountNumber, FiscalYear, FiscalPeriod, Form, FilingDate, Frame]
+                                cfacts.append((cik, entity, field, obj.get("end", None), obj.get("val", None), obj.get("accn", None), obj.get("fy", None), obj.get("fp", None), obj.get("form", None), obj.get("filed", None), obj.get("frame", None)))
+                #print(f"\n\n")
+
+        fdata = pd.DataFrame(cfacts, columns=["CIK", "EntityName", "Field", "Timestamp", "Value", "AccountNumber", "FiscalYear", "FiscalPeriod", "Form", "FilingDate", "Frame"])
+        print(fdata.head())
+
+        self.ef.add_to_sheet(fdata, sheet_name=f"{entity}_FACTS")
 
     def fetch_sec_filing(self, ticker: str) -> bytes:
         """
@@ -67,17 +112,13 @@ class SEC():
         :param cik_list: List of 10-digit CIK strings.
         """
         cik = self.cik_map[ticker]
-        aggregated_filings = {}
         filings_data = self.extract_data(cik)
         print(filings_data, "\n\n")
         if filings_data:
-            aggregated_filings[cik] = filings_data
+            return filings_data
         else:
-            print(f"No data found for CIK: {cik}")
+            print(f"No data found for ticker: {ticker}")
 
-        # Save aggregated data
-        #spath = os.path.join(self.base_dir, f'data/filings_{datetime.datetime.now().strftime('%Y%m%d')}_{datetime.datetime.now().strftime('%H%M%S')}.json')
-        #save_json(spath, aggregated_filings)
 
     def fetch_accounts_payable(self, cik: str) -> bytes:
         """
@@ -95,13 +136,14 @@ class SEC():
 
     def extract_data(self, cik: str) -> bytes:
         print(f"\n[REGI] - Extracting data for the following CIK (Central Index Key): {cik}\n")
-#        url = self.url_template.replace('##########', cik)
         url = self.url_xbrl.replace('##########', cik)
 
         print(url)
         res = self.reqsesh.get(url)
 
         return res
+
+    
 
 
 if __name__=="__main__":
---
+++ utils/excel_formatter.py
@@ -0,0 +1,149 @@
+"""
+This class will be used to take in a DataFrame and then format it as an Excel file seamlessly. 
+
+You will be able to store the file in some output directory, and if not provided, it will be stored in your local directory in some folder 'output'.
+
+You can also return a reference to the original data to perform some logical manipulation (i.e color coding, data transformation, aggregation, etc...)
+"""
+
+import sys, os
+import pandas as pd
+import openpyxl 
+from openpyxl import Workbook
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.utils import get_column_letter
+from openpyxl.utils.dataframe import dataframe_to_rows
+import datetime
+import pathlib
+
+
+class ExcelFormatter:
+    def __init__(self):
+     # Create a reference to an empty Excel workbook
+        self.wb = Workbook()
+     
+
+    def add_to_sheet(self, df: pd.DataFrame, sheet_name: str, transform_fn = None) -> None:
+        """
+        Pass in a dataframe object which will be added to a sheet in an Excel file object with a specific sheet name.\n
+
+        :param df: The dataframe object which will be saved to the sheet
+        :param sheet_name: The name of the sheet which will contain the data in df
+        :param transform_fn: [IN PROGRESS] Add a lambda function which will take in a df as a paramater for pre-transformation prior to saving the data
+        """
+
+        ws = self.wb.active
+     # Check if the default sheet is empty
+        is_empty = all([cell.value is None for row in ws.iter_rows() for cell in row])
+
+        if is_empty:
+         # Use the default sheet
+            ws.title = sheet_name
+        else:
+         # Create a new sheet
+            ws = self.wb.create_sheet(title=sheet_name)
+
+     # Apply transformation to the dataframe
+        if transform_fn:
+            df = transform_fn(df)
+
+     # Open up the sheet in the workbook
+        if sheet_name in self.wb.sheetnames:
+            ws = self.wb[sheet_name] # Open up the sheet if it already exists 
+        else:
+            ws = self.wb.create_sheet(title=sheet_name)
+
+     # Save the rows in the dataframe
+        for row in dataframe_to_rows(df, index=False, header=True):
+            ws.append(row)
+
+     # Get metadata on rows and columns
+        num_rows = ws.max_row
+        num_cols = ws.max_column
+        table_ref = f"A1:{get_column_letter(num_cols)}{num_rows}"
+
+     # Create and style table
+        table = Table(displayName="".join(sheet_name.split(" ")), ref=table_ref)
+        style = TableStyleInfo(
+            name="TableStyleMedium9",
+            showFirstColumn=False,
+            showLastColumn=False,
+            showRowStripes=True,
+            showColumnStripes=False
+        )
+        table.tableStyleInfo = style 
+        ws.add_table(table)
+
+     # Format the column widths according to the size of the data in the rows
+        for i, col in enumerate(df.columns, start=1):
+            col_letter = get_column_letter(i)
+            max_len = max(len(str(cell)) for cell in [col] + df[col].astype(str).tolist())
+            ws.column_dimensions[col_letter].width = max_len + 4
+
+    def save(self, filename: str, location: str = None) -> None:
+        """
+        This method will take in a workbook object and then save it to a file location specified by 'location' \n
+        If the 'location' parameter is empty, then it will store the output file to a folder called 'output' located in the relative directory. 
+
+        :param filename: The name of the output Excel file which contains the resultant set from DB calls
+        :param location: [OPTIONAL] The location in which the Excel file will be stored in the context of your directory structure. 
+        """
+     # Craft the full location path of the saved output file
+        fpath = location if location else self.__create_output_dir()
+
+     # Validate the filename to ensure it is .xlsx 
+        path_split = filename.split('.')[1]
+        if not path_split:
+            print(f"There is no extension to the filename provided. \n\tPlease create a filename with the following example convention: example.xlsx")
+            self.__reset_workbook()
+            return None 
+        elif path_split != 'xlsx':
+            print(f"The file extension is '{path_split}' which is NOT of type 'xlsx'. Please correct it.")
+            self.__reset_workbook()
+            return None
+
+     # Validate the save location to ensure it exists
+        if not os.path.exists(fpath):
+            print(f"The following file location does not exist: {location}. \n\tPlease enter a valid file location. ")
+            self.__reset_workbook()
+            return None
+
+     # Save the file to the specified location if it exists
+        spath = os.path.join(fpath, filename)
+        self.wb.save(spath)
+        self.__reset_workbook()
+
+        print(f"Saved the output workbook succesfully to {spath}\n")
+
+    def __create_output_dir(self) -> str:
+        """
+        Create a folder called 'output' in the relative directory.\n
+        To be used in the 'save' method as a means to create the output directory seamlessly
+        """
+
+     # Get the base directory path 
+        base_dir = str(pathlib.Path(__file__).parent)
+     # Join the base_dir and the 'output' folder
+        o_dir = base_dir + '\output'
+     # Create a folder called 'output' in the base_dir
+        if not os.path.exists(o_dir):
+            try:
+                os.mkdir(o_dir)
+            except FileExistsError:
+                print(f"Directory {o_dir} already exists.")
+                pass # Continue with the logical flow
+            except PermissionError:
+                print(f"Permission denied: Unable to create '{o_dir}'")
+                self.__reset_workbook()
+                return None
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                self.__reset_workbook()
+                return None
+
+     # Return the output directory path 
+        return os.path.realpath(o_dir)
+
+    def __reset_workbook(self):
+        print(f"Resetting the Excel workbook...")
+        self.wb = Workbook()
